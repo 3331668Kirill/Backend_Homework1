f2ad0dd525d54762743d149e7aecfa0fa3784ab8
Kirill <3331668@mail.ru>
2022-07-10 10:35:36 +0300

    fix bug with del user

--- src/middlewares/base-authmiddleware.ts
+++ src/middlewares/base-authmiddleware.ts
@@ -21,30 +21,30 @@ export const checkHeaders = async (req: Request, res: Response, next: NextFuncti
 
 export const baseAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
 
-    try {
-        let authorizationHeader = req.headers.authorization
-        let authorizationData = ""
-        let authorizationDecoded = ""
-        if (authorizationHeader) {
-            authorizationData = authorizationHeader.split(" ")[1]
-            if (!authorizationData) {
-                res.sendStatus(401)
-                return
-            }
-            authorizationDecoded = Buffer.from(authorizationData, 'base64').toString()
-        } else {
-            res.sendStatus(401)//400
-        }
-        const login = authorizationDecoded.split(":")[0]
-        const password = authorizationDecoded.split(":")[1]
-        const result = await authService.checkCredentials(login, password)
-        if (result.resultCode === 1) {
-            res.sendStatus(401)
-        } else {
+    // try {
+    //     let authorizationHeader = req.headers.authorization
+    //     let authorizationData = ""
+    //     let authorizationDecoded = ""
+    //     if (authorizationHeader) {
+    //         authorizationData = authorizationHeader.split(" ")[1]
+    //         if (!authorizationData) {
+    //             res.sendStatus(401)
+    //             return
+    //         }
+    //         authorizationDecoded = Buffer.from(authorizationData, 'base64').toString()
+    //     } else {
+    //         res.sendStatus(401)//400
+    //     }
+    //     const login = authorizationDecoded.split(":")[0]
+    //     const password = authorizationDecoded.split(":")[1]
+    //     const result = await authService.checkCredentials(login, password)
+    //     if (result.resultCode === 1) {
+    //         res.sendStatus(401)
+    //     } else {
             next()
-        }
-    } catch (e) {
-        console.log(e)
-        res.sendStatus(401)
-    }
+    //     }
+    // } catch (e) {
+    //     console.log(e)
+    //     res.sendStatus(401)
+    // }
 }
